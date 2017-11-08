40bce34b25be215a159755c6a8428522cb7fe637
Red-Portal <msca8h@naver.com>
2017-11-08 20:18:16 +0900

    initial commit

---
+++ build.sh
@@ -0,0 +1,4 @@
+cmake \
+    -DCUDA_ARCH=61\
+    -DCUDA_HOST_COMPILER=/usr/bin/g++-6 \
+    -G "Unix Makefiles" ./mgcpp
---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include <mgcpp/device/vector.hpp>
+
+int main()
+{
+    mgcpp::device_vector<float> vec;
+}

bd1cd071105efa84234bc93d0be866184684a9f7
Red-Portal <msca8h@naver.com>
2017-12-03 23:15:23 +0900

    moved implementation to header

---
+++ LC_DG_update.cpp
@@ -0,0 +1,174 @@
+
+#include <optional>
+
+#include "gradient_projection.hpp"
+#include "LC_DG_update.hpp"
+
+template<typename d_F, typename bestL>
+inline std::optional<nice::row_vector>
+Pg2_update(float descent_rate,
+           float kkt_threshold,
+           nice::row_vector const& Pg2,
+           d_F& d_Pg2,
+           bestL& best_lambda,
+           nice::matrix const& A,
+           nice::column_vector const& b)
+{
+    auto Pg2_gradient = d_Pg2(Pg2);
+    auto next_Pg2 = descent_rate * Pg2_gradient;
+
+    auto active = nice::active_constraints(A, b, next_Pg2);
+
+    if(!active)
+        return next_Pg2;
+    else
+    {
+        auto P = nice::projection_matrix(active.value());
+        auto s = (-1) * Pg2_gradient * blaze::trans(P);
+
+        if(nice::norm_l2(s) < kkt_threshold)
+            return {};
+
+        float best = best_lambda(Pg2, s);
+        float max = nice::max_lambda(A, b, Pg2, s);
+        auto update = blaze::evaluate(std::min(best, max) * s);
+
+        if(nice::norm_l2(update) < kkt_threshold)
+            return {};
+
+        return update;
+    }
+}
+
+std::tuple<nice::matrix, nice::column_vector>
+LC_DG_constraints()
+{
+    auto constraints = nice::matrix(23 + 24, 24, 0);
+
+    for(auto i = 0u; i < 23u; ++i)
+    {
+        constraints(i, i) = 1;
+        constraints(i, i + 1) = -1;
+    }
+
+    for(auto i = 0u; i < 24u; ++i)
+        constraints(i + 23, i) = 1; 
+ 
+    auto constraint_range = nice::column_vector(23 + 24, 0);
+
+    for(auto i = 0u; i < 23; ++i)
+        constraint_range[i] = 1.8;
+
+    return {constraints, constraint_range};
+}
+
+std::tuple<nice::row_vector, nice::row_vector>
+LC_DG_optimization(float descent_rate,
+                   size_t max_iteration,
+                   float alphag,
+                   float betag,
+                   float cg,
+                   float gamma,
+                   blaze::DynamicMatrix<float> const& Pk,
+                   blaze::DynamicMatrix<float> const& Qk,
+                   nice::row_vector const& mu2,
+                   nice::row_vector const& lambda2,
+                   float kkt_threshold,
+                   bool verbose)
+{
+    bool Pg2_found = false;
+    bool Qg2_found = false;
+
+    auto Pg2 = nice::row_vector(24, 1);
+    auto Qg2 = nice::row_vector(24, 1);
+
+    // cashed constants
+    auto betag_constant = nice::row_vector(24, betag);
+    auto cg_constant = nice::row_vector(24, cg);
+
+    std::cout << "LC_LG" << std::endl;
+
+    auto objective = [&](nice::row_vector const& Pg,
+                         nice::row_vector const& Qg)
+        {
+            auto Cg_diesel2 = nice::sum(alphag * nice::power(Pg) + betag * Pg + cg_constant);
+
+            auto P_inner = mu2 - Pg - row(Pk, 0);
+            auto Q_inner = lambda2 - Qg - row(Qk, 0);
+
+            auto P = (1/(2 * gamma)) * dot(P_inner, P_inner);
+            auto Q = (1/(2 * gamma)) * dot(Q_inner, Q_inner);
+
+            return Cg_diesel2 + P + Q;
+        };
+
+    auto d_Pg2 = [&](nice::row_vector const& Pg)
+        {
+            auto P_inner = mu2 - Pg - row(Pk, 0);
+            auto Cg_diesel2_derivative = 2 * alphag * Pg + betag_constant;
+            return blaze::evaluate(descent_rate * (Cg_diesel2_derivative + (-1 / gamma) * (P_inner)));
+        };
+
+    auto d_Qg2 = [&](nice::row_vector const& Qg)
+        {
+            auto Q_inner = lambda2 - Qg - row(Qk, 0);
+            return blaze::evaluate(descent_rate * (-1 / gamma) * (Q_inner));
+        };
+
+    auto best_lambda = [&](nice::row_vector const& Pg,
+                           nice::row_vector const& s)->float
+        {
+            auto dividend = 2 * alphag * dot(Pg, s) - betag * dot(s, (Pg + row(Pk, 0) - mu2));
+            auto dividor = dot(s, s) * (2 * alphag + (1 / gamma));
+            return dividend / dividor;
+        };
+
+    auto [A, b] = LC_DG_constraints();
+
+    float objective_value = 0;
+
+    for(auto i = 0u; i < max_iteration; ++i)
+    {                                       
+        if(verbose)
+            objective_value = objective(Pg2, Qg2);
+
+        if(Pg2_found && Qg2_found)
+            break;
+
+        if(!Qg2_found)
+        {
+            auto Qg2_gradient = d_Qg2(Qg2);
+
+            std::cout << "delta Qg2: "<< Qg2_gradient;
+
+            if(nice::norm_l2(Qg2_gradient) < kkt_threshold)
+                Qg2_found = true;
+            else
+                Qg2 -= descent_rate * Qg2_gradient;
+        }
+
+        if(!Pg2_found)
+        {
+            auto result = Pg2_update(descent_rate,
+                                     kkt_threshold,
+                                     Pg2,
+                                     d_Pg2,
+                                     best_lambda,
+                                     A,
+                                     b);
+            if(result)
+                Pg2 = result.value();
+            else
+                Pg2_found = true;
+        }
+
+        if(verbose)
+        {
+            std::cout << "iteration: " << i + 1
+                      << " objective: " << objective_value
+                      << '\n';
+        }
+    }
+
+    return {Pg2, Qg2};
+} 
--- LC_DG_update.hpp
+++ LC_DG_update.hpp
@@ -7,81 +7,19 @@
 #include <algorithm>
 
 #include "utility.hpp"
-#include "gradient_projection.hpp"
 
-inline std::tuple<row_vector, row_vector>
+std::tuple<nice::row_vector, nice::row_vector>
 LC_DG_optimization(float descent_rate,
+                   size_t max_iteration,
                    float alphag,
                    float betag,
                    float cg,
                    float gamma,
                    blaze::DynamicMatrix<float> const& Pk,
                    blaze::DynamicMatrix<float> const& Qk,
-                   row_vector const& mu2,
-                   row_vector const& lambda2)
-{
-    auto Pg2 = row_vector(24, 1);
-    auto Qg2 = row_vector(24, 1);
-
-    float LC_DG_loss = std::numeric_limits<float>::max();
-
-    // cashed constants
-    auto betag_constant = row_vector(24, betag);
-    auto cg_constant = row_vector(24, cg);
-
-    std::cout << "LC_LG" << std::endl;
-
-    auto function = [&](nice::row_vector const& Pg2)
-        {
-
-        };
-
-    auto d_function = []()
-        {
-            
-        };
-
-    auto best_lambda = []()
-        {
-        
-        };
-
-
-    for(auto i = 0u; i < 27; ++i)
-    {                                       
-        auto Cg_diesel2 = nice::sum(alphag * nice::power(Pg2) + betag * Pg2 + cg_constant);
-
-        auto P_inner = blaze::evaluate((-1 * Pg2) - row(Pk, 0) + mu2);
-        auto Q_inner = blaze::evaluate((-1 * Qg2) - row(Qk, 0) + lambda2);
-            
-        auto P = (1/(2 * gamma)) * dot(P_inner, P_inner);
-        auto Q = (1/(2 * gamma)) * dot(Q_inner, Q_inner);
-        LC_DG_loss = Cg_diesel2 + P + Q;
-
-        auto Cg_diesel2_derivative = 2 * alphag * Pg2 + betag_constant;
-
-        auto Pg_delta = descent_rate * (Cg_diesel2_derivative + (-1 / gamma) * (P_inner));
-        auto Qg_delta = descent_rate * (-1 / gamma) * (Q_inner);
-
-        //std::cout << "diesel delta " << Cg_diesel2_derivative[0] << std::endl;
-        //std::cout << "diesel " << Cg_diesel2 << std::endl;
-
-        //std::cout << "Pg delta " << Pg_delta[0] << std::endl;
-        //std::cout << "Qg delta " << Qg_delta[0] << std::endl;
-
-        //std::cout << "Pg2 " << Pg2[0] << std::endl;
-        //std::cout << "Qg2 " << Qg2[0] << std::endl;
-
-        std::cout << "loss: " << LC_DG_loss << std::endl;
-
-        Pg2 = P_optimizer.update(Pg2, Pg_delta);
-        Qg2 = Q_optimizer.update(Qg2, Qg_delta);
-
-        //std::cout << "New Pg2 " << Pg2[0] << std::endl;
-        //std::cout << "New Qg2 " << Qg2[0] << std::endl;
-    }
-
-    return {Pg2, Qg2};
-} 
+                   nice::row_vector const& mu2,
+                   nice::row_vector const& lambda2,
+                   float kkt_threshold = 0.0001,
+                   bool verbose = true);
 
 #endif

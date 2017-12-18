f77faeeb6178790210287dc5b22fd727ce3c89df
Red-Portal <msca8h@naver.com>
2017-12-18 23:25:51 +0900

    updated project infrastructure

--- utility.hpp
+++ utility.hpp
@@ -2,6 +2,10 @@
 #ifndef _NICE_UTILITY_HPP_
 #define _NICE_UTILITY_HPP_
 
+#define BLAZE_BLAS_MODE 1
+#define BLAZE_BLAS_IS_PARALLEL 1
+#define BLAZE_USE_VECTORIZATION 1
+
 #include <blaze/Blaze.h>
 
 namespace nice
@@ -10,6 +14,12 @@ namespace nice
     using sparse_matrix = blaze::CompressedMatrix<float>;
     using column_vector = blaze::DynamicVector<float, blaze::columnVector>; 
     using row_vector = blaze::DynamicVector<float, blaze::rowVector>; 
+
+    enum class verboseness : size_t {quiet = 0,
+                                     verbose = 1,
+                                     very_verbose = 2,
+                                     log = 3};
+
 
     inline float
     sum(blaze::DynamicMatrix<float> const& mat)
@@ -43,16 +53,16 @@ namespace nice
         return sum(mat) / (mat.rows() * mat.columns());
     }
 
-    template<bool Allign>
-    inline blaze::DynamicVector<float, Allign>
-    power(blaze::DynamicVector<float, Allign> const& vec)
+    template<typename VT, bool TF>
+    inline decltype(auto)
+    power(blaze::Vector<VT, TF> const& vec)
     {
         return vec * vec;
     }
 
     template< typename VT, bool TF >
     inline decltype(auto)
-    norm_l2( const blaze::Vector<VT,TF>& vec )
+    norm_l2( blaze::Vector<VT,TF> const& vec )
     {
         return sqrt( blaze::dot( ~vec, ~vec ) );
     }
